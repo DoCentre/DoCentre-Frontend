c1500c11e6dca7207b350e444873b9fef2563525
Tsai chi-chien <aura.cs12@nycu.edu.tw>
2024-05-02 14:23:35 +0800

    feat: init doc (#2)

--- Frontend/src/api/docApi.js
+++ Frontend/src/api/docApi.js
@@ -13,3 +13,45 @@ export const createDoc = async function (userID) {
     return null;
   }
 };
+
+export const initDoc = async function (
+  appendix,
+  approverID,
+  authorID,
+  content,
+  documentID,
+  status,
+  title
+) {
+  let json = {
+    appendix: appendix,
+    approver_id: approverID,
+    author_id: authorID,
+    content: content,
+    document_id: documentID,
+    status: status,
+    title: title,
+  };
+
+  try {
+    const response = await axios.put("/api/document/update", json);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export const getDocList = async function (userID) {
+  let json = {
+    author_id: userID,
+  };
+
+  try {
+    const response = await axios.post("/api/documents/author", json);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
