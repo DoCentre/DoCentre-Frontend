c4d8c3fbca306901ff11256e892692579603815e
AU2A <aura.cs12@nycu.edu.tw>
2024-05-03 10:30:14 +0800

    add temp route test page

--- Frontend/src/views/temp.vue
+++ Frontend/src/views/temp.vue
@@ -3,3 +3,12 @@
         123
     </v-container>
 </template>
+
+<script>
+export default {
+    name: "temp",
+    async created() {
+        console.log(this.$route.params.id)
+    },
+};
+</script>
