ed54d4779061e042935cba0fea67d047a4baea08
AU2A <aura.cs12@nycu.edu.tw>
2024-05-03 18:02:45 +0800

    dropdown doc history

--- Frontend/src/views/AdminView.vue
+++ Frontend/src/views/AdminView.vue
@@ -28,54 +28,48 @@
                         </v-col>
                     </v-row>
                     <template v-slot:append>
-                        <v-btn color="primary" text="Create goal" variant="text" @click="test"></v-btn>
+                        <v-btn color="primary" text="Edit" variant="text" @click="test"></v-btn>
                     </template>
                 </v-card-item>
 
                 <v-expand-transition>
                     <div v-if="expand">
-                        <v-list>
-
-                        </v-list>
-                    </div>
-                </v-expand-transition>
-            </v-card>
-        </v-col>
-        <v-col cols="12">
-            <v-card class="mx-auto" hover @click="expand = !expand;">
-                <v-card-item class="text-h5">
-                    <v-card-title>Title</v-card-title>
-                    <v-row>
-                        <v-col cols="2">
-                            <v-card-text>
-                                Editor: SomeOne
-                            </v-card-text>
-                        </v-col>
-                        <v-col cols="2">
-                            <v-card-text>
-                                Approver: SomeOne
-                            </v-card-text>
-                        </v-col>
-                        <v-col cols="3">
-                            <v-card-text>
-                                Last Edit time: 2021-09-01 12:00:00
-                            </v-card-text>
-                        </v-col>
-                        <v-col cols="2">
-                            <v-card-text>
-                                Status
-                            </v-card-text>
-                        </v-col>
-                    </v-row>
-                    <template v-slot:append>
-                        <v-btn color="primary" text="Create goal" variant="text" @click="test"></v-btn>
-                    </template>
-                </v-card-item>
-
-                <v-expand-transition>
-                    <div v-if="expand">
-                        <v-list>
-
+                        <v-list lines="two">
+                            <v-list-subheader>File History</v-list-subheader>
+                            <v-list-item-group>
+                                <v-list-item>
+                                    <v-row>
+                                        <v-col>
+                                            2021-09-01 12:00:00
+                                        </v-col>
+                                        <v-col>
+                                            Editor: SomeOne
+                                        </v-col>
+                                        <v-col>
+                                            Approver: SomeOne
+                                        </v-col>
+                                        <v-col>
+                                            Status: Pending
+                                        </v-col>
+                                    </v-row>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-row>
+                                        <v-col>
+                                            2021-09-01 12:00:00
+                                        </v-col>
+                                        <v-col>
+                                            Editor: SomeOne
+                                        </v-col>
+                                        <v-col>
+                                            Approver: SomeOne
+                                        </v-col>
+                                        <v-col>
+                                            Status: Pending
+                                        </v-col>
+                                    </v-row>
+                                </v-list-item>
+                            </v-list-item-group>
                         </v-list>
                     </div>
                 </v-expand-transition>
