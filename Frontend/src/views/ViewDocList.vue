dd10f17c02a56ff1931392ac3c21a8c0a3d52257
Tsai chi-chien <aura.cs12@nycu.edu.tw>
2024-05-04 17:46:37 +0800

    Feat/sort doc by status and time (#18)
    * change storage from color to level
    
    * use level to control color
    
    * sort card by level and time

--- Frontend/src/views/ViewDocList.vue
+++ Frontend/src/views/ViewDocList.vue
@@ -5,7 +5,7 @@
             <v-layout row wrap v-for="(doc) in docs" :key="doc.id">
                 <v-col>
                     <v-card class="mx-auto" :title="doc.title" :subtitle="doc.status" hover max-width="400"
-                        :color="doc.color" @click="check(doc.id)">
+                        :color="color[doc.level]" @click="check(doc.id)">
                         <v-card-text>
                             {{ doc.time }}<br>
                             {{ doc.date }}
@@ -27,6 +27,7 @@ export default {
     },
     data() {
         return {
+            color: ["green", "yellow", "gray", "red"],
             docs: [],
         };
     },
@@ -42,13 +43,16 @@ export default {
             return {
                 id: doc["id"],
                 title: doc["title"] || "untitled",
-                color: doc["status"] === "EDIT" ? "gray" : doc["status"] === "VERIFY" ? "yellow" : doc["status"] === "REJECT" ? "red" : "green",
+                level: doc["status"] === "EDIT" ? 2 : doc["status"] === "VERIFY" ? 1 : doc["status"] === "REJECT" ? 3 : 0,
                 status: doc["status"],
                 date: new Date(new Date(doc["updated_at"]).getTime()).toLocaleDateString(),
                 time: new Date(new Date(doc["updated_at"]).getTime()).toLocaleTimeString([], { hour12: false }),
             };
         });
         this.docs.sort((a, b) => {
+            if (a.level !== b.level) {
+                return b.level - a.level;
+            }
             return new Date(b.date + " " + b.time) - new Date(a.date + " " + a.time);
         });
     },
