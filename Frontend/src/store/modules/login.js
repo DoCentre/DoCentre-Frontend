e1041a86841df93b9dd191d88fce7a4fe1e9253a
Aura <aura.cs12@nycu.edu.tw>
2024-05-18 09:57:54 +0800

    remove unused storage

--- Frontend/src/store/modules/login.js
+++ Frontend/src/store/modules/login.js
@@ -13,8 +13,6 @@ const getDefaultState = () => {
     ? {
         id: parseInt(localStorage.getItem("id")),
         username: localStorage.getItem("username"),
-        identity: localStorage.getItem("identity"),
-        email: localStorage.getItem("email"),
         isLogin: true,
       }
     : blankUserData;
@@ -31,16 +29,12 @@ const actions = {
     if (userData !== null) {
       localStorage.setItem("id", userData.user.id);
       localStorage.setItem("username", userData.user.username);
-      localStorage.setItem("identity", userData.user.identity);
-      localStorage.setItem("email", userData.user.email);
       commit("updateUserData", userData.user);
     }
   },
   logoutUser({ commit }) {
     localStorage.removeItem("id");
     localStorage.removeItem("username");
-    localStorage.removeItem("identity");
-    localStorage.removeItem("email");
     commit("updateUserData", blankUserData);
   },
 };
