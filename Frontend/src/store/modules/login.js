15b977615cbd55e780a9861f7a643125805ca734
Tsai chi-chien <aura.cs12@nycu.edu.tw>
2024-05-18 09:59:01 +0800

    Fix/change session storage type (#35)
    * change sessionStorage to localStorage
    
    * remove unused storage

--- Frontend/src/store/modules/login.js
+++ Frontend/src/store/modules/login.js
@@ -9,12 +9,10 @@ const blankUserData = {
 };
 
 const getDefaultState = () => {
-  return sessionStorage.getItem("username")
+  return localStorage.getItem("username")
     ? {
-        id: parseInt(sessionStorage.getItem("id")),
-        username: sessionStorage.getItem("username"),
-        identity: sessionStorage.getItem("identity"),
-        email: sessionStorage.getItem("email"),
+        id: parseInt(localStorage.getItem("id")),
+        username: localStorage.getItem("username"),
         isLogin: true,
       }
     : blankUserData;
@@ -29,18 +27,14 @@ const actions = {
     const userData = await loginUser(loginData.username, loginData.password);
 
     if (userData !== null) {
-      sessionStorage.setItem("id", userData.user.id);
-      sessionStorage.setItem("username", userData.user.username);
-      sessionStorage.setItem("identity", userData.user.identity);
-      sessionStorage.setItem("email", userData.user.email);
+      localStorage.setItem("id", userData.user.id);
+      localStorage.setItem("username", userData.user.username);
       commit("updateUserData", userData.user);
     }
   },
   logoutUser({ commit }) {
-    sessionStorage.removeItem("id");
-    sessionStorage.removeItem("username");
-    sessionStorage.removeItem("identity");
-    sessionStorage.removeItem("email");
+    localStorage.removeItem("id");
+    localStorage.removeItem("username");
     commit("updateUserData", blankUserData);
   },
 };
