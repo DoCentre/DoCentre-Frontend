fd3e199d3d75e54cef17b10dfc00903ab672f21b
AU2A <aura.cs12@nycu.edu.tw>
2024-05-24 12:18:59 +0800

    show main info

--- Frontend/src/views/showMetrics.vue
+++ Frontend/src/views/showMetrics.vue
@@ -1,34 +1,58 @@
 <template>
     <NavigationBar />
     <v-container>
-        <v-layout v-for="metric in metrics" :key="metric.api">
-            <v-col>
-                <h2>{{ metric.api }}</h2>
-                <h3>{{ metric.info }}</h3>
-                <v-table>
-                    <thead>
-                        <tr>
-                            <th class="text-left" width="30%">
-                                Type
-                            </th>
-                            <th class="text-left">
-                                {{ metric.type }}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="data in metric.data" :key="data[0]">
-                            <td class="text-left">
-                                {{ data[0] }}
-                            </td>
-                            <td class="text-left">
-                                {{ data[1] }}
-                            </td>
-                        </tr>
-                    </tbody>
-                </v-table>
-            </v-col>
-        </v-layout>
+        <h1>System Status</h1>
+        <h2>Memory Usage: {{ memory.toFixed(2) }} MB / 1000 MB</h2>
+        <div id="myProgress">
+            <div id="myBar" :style="{ width: memoryPercent }"></div>
+        </div>
+        <br>
+        <h2>Process Time: {{ processTime.toFixed(2) }} ms / 1 ms</h2>
+        <div id="myProgress">
+            <div id="myBar" :style="{ width: processTimePercent }"></div>
+        </div>
+        <br>
+        <h2>HTTP Traffic</h2>
+        <br>
+        <h3>Total : {{ http200 + http500 + http503 }}</h3>
+        <br>
+        <h3>HTTP 200: {{ http200 }}</h3>
+        <br>
+        <h3>HTTP 500: {{ http500 }}</h3>
+        <br>
+        <h3>HTTP 503: {{ http503 }}</h3>
+        <br>
+        <br>
+        <v-btn @click="showDetail = !showDetail">Show Detail</v-btn>
+        <div v-if="showDetail">
+            <v-layout v-for="metric in metrics" :key="metric.api">
+                <v-col>
+                    <h3>{{ metric.info }}</h3>
+                    <v-table>
+                        <thead>
+                            <tr>
+                                <th class="text-left" width="30%">
+                                    Type
+                                </th>
+                                <th class="text-left">
+                                    {{ metric.type }}
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="data in metric.data" :key="data[0]">
+                                <td class="text-left">
+                                    {{ data[0] }}
+                                </td>
+                                <td class="text-left">
+                                    {{ data[1] }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+                </v-col>
+            </v-layout>
+        </div>
     </v-container>
 </template>
 
@@ -42,7 +66,15 @@ export default {
     },
     data() {
         return {
+            showDetail: false,
             metrics: [],
+            memory: 0,
+            memoryPercent: 0,
+            processTime: 0,
+            processTimePercent: 0,
+            http200: 0,
+            http500: 0,
+            http503: 0,
         };
     },
     async created() {
@@ -65,7 +97,29 @@ export default {
                 data: data,
             });
         }
-        // console.log(this.metrics);
+        this.memory = Number(this.metrics[30]["data"][0][1]) / 1024 / 1024;
+        this.memoryPercent = String(this.memory / 10) + "%";
+        this.http200 = Number(this.metrics[35]["data"][0][1]);
+        this.http500 = Number(this.metrics[35]["data"][1][1]);
+        this.http503 = Number(this.metrics[35]["data"][2][1]);
+        this.processTime = Number(this.metrics[27]["data"][0][1]) / (this.http200 + this.http500 + this.http503);
+        this.processTimePercent = String(this.processTime * 100) + "%";
+        console.log(this.metrics);
     },
 };
 </script>
+
+<style>
+#myProgress {
+    width: 100%;
+    background-color: #ddd;
+}
+
+#myBar {
+    height: 30px;
+    background-color: #04AA6D;
+    text-align: center;
+    line-height: 30px;
+    color: white;
+}
+</style>
